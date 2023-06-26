91d34909bc1e3d1c39a69ed690cdb22e910bafa2
Diego <diegosousa241104@gmail.com>
2023-06-26 18:53:54 -0300

    :recycle: Rename toast function and add user registration function to sumbit event

--- public/js/user/register.js
+++ public/js/user/register.js
@@ -1,6 +1,9 @@
 const form = document.forms[0];
 
-form.addEventListener('submit', (event) => event.preventDefault());
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	registerUser();
+});
 
 async function registerUser() {
 	const { name, email, password, confirmPassword } = form;
@@ -27,31 +30,31 @@ async function registerUser() {
 		if(response.ok)
 			createToast('Usuário cadastrado com sucesso!', 'fe:check', '#006432');
 		else if (data.code === 'ER_DUP_ENTRY')
-			displayToast('Esse email já está cadastrado no sistema!');
+			showErrorToast('Esse email já foi cadastrado no sistema!');
 		else 
-			displayToast('Não foi possível efetuar o cadastro!');
+			showErrorToast('Não foi possível efetuar o cadastro!');
 	}
 }
 
 function inputsAreValid(name, email, password, confirmPassword) {
 	if(!name || !email || !password || !confirmPassword) {
-		displayToast('Preencha todos os campos!');
+		showErrorToast('Preencha todos os campos!');
 		return false;
 	}
 
 	if (password !== confirmPassword) {
-		displayToast('As senhas não coincidem!');
+		showErrorToast('As senhas não coincidem!');
 		return false;
 	}
 	
 	if (password.length < 8) {
-		displayToast('Sua senha deve ter pelo menos 8 caracteres!');
+		showErrorToast('Sua senha deve ter pelo menos 8 caracteres!');
 		return false;
 	}
 	
 	return true;
 }
 
-function displayToast(message) {
+function showErrorToast(message) {
 	createToast(message, 'typcn:warning', '#E7584F');
 }
