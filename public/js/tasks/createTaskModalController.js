ce0a1ad5a19ef250cbda7ac27fd0ab5e678234f8
Diego <diegosousa241104@gmail.com>
2023-07-05 12:34:05 -0300

    :lipstick: feat: Add create task modal in block screen

---
+++ public/js/tasks/createTaskModalController.js
@@ -0,0 +1,20 @@
+const addTaskButton = document.getElementById('addTaskButton');
+const createTaskModal = document.getElementById('createTaskModal');
+const closeModalButton = document.getElementById('closeModalButton');
+
+function openModal(column) {
+	createTaskModal.style.display = 'flex';
+	
+	if(column == 'TO_DO')
+		createTaskModal.querySelector('h1').innerHTML = 'Adicionar tarefa a ser feita';
+	else if(column == 'IN_PROGRESS')
+		createTaskModal.querySelector('h1').innerHTML = 'Adicionar tarefa em andamento';
+	else 
+		createTaskModal.querySelector('h1').innerHTML = 'Adicionar tarefa já finalizada';
+}
+
+function closeModal() {
+	createTaskModal.style.display = 'none';
+}
+
+closeModalButton.addEventListener('click', closeModal);
